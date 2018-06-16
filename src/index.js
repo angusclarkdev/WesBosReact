b2f1407a98808d458723e33e86a1cfdc3c19d229
Angus Clark <angusclark@anguss-mbp.home>
2018-06-16 23:38:38 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+import "./css/style.css";
+import App from './components/App';
+
+render( <App />, document.getElementById('main'));
